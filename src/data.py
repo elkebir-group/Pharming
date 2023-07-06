d436c638f1d6d7159a3a12fd676cdb678b4a927c
Leah Weber <leahlw2@ada.cs.illinois.edu>
2023-07-06 13:06:47 -0500

    added preprocessing of input data and added helper functions to data class

--- src/data.py
+++ src/data.py
@@ -14,7 +14,47 @@ G = number of segments
 class Data:
     var : np.array  # N x M matrix with the number of variant read counts
     total : np.array #  N x M matrix with the number of total read counts
-    copy_numbers: np.array # N x G matrix with the copy number profile of each cell in each segment
-    snv_to_seg: np.array  # an M length array which holds the segment of an SNV 
-    cell_lookup : pd.Series # an N length series mapping internal cell index to input cell label
-    mut_lookup : pd.Series #an M length series mapping interal SNV index to input SNV label
+    copy_numbers: np.array# N x G matrix with the copy number profile of each cell in each segment
+    snv_to_seg: dict  # a dictionary that maps each snv to seg
+    seg_to_snvs: dict #a ditionary that maps each seg to a list of snvs in that segment
+    # cell_lookup : pd.Series # an N length series mapping internal cell index to input cell label
+    # mut_lookup : pd.Series #an M length series mapping interal SNV index to input SNV label
+
+    def __post_init__(self):
+        self.nseg = len(self.seg_to_snvs)
+        self.N, self.M = self.var.shape
+        self.segments = list(self.seg_to_snvs.keys())
+        self.segments.sort()
+        self.cells = np.arange(self.N)
+        self.muts = np.arange(self.M)
+    
+    def __str__(self):
+        mystr= f"Input data contains: {self.N} cells, {self.M} SNVs and {self.nseg} segments"
+        return mystr
+
+    def compute_vafs(self, cells=None, snvs=None):
+        if cells is None:
+            cells = self.cells 
+        if snvs is None:
+            snvs = self.muts 
+        
+        var =self.var[np.ix_(cells, snvs)]
+        total = self.total[np.ix_(cells, snvs)]
+        return var.sum(axis=0)/total.sum(axis=0)
+    
+    def compute_likelihood(self):
+        pass 
+
+    def cells_by_cn(self, seg):
+        arr= self.copy_numbers[:,seg]
+        mapping = {}
+        unique_values, inverse_indices, value_counts = np.unique(arr, return_inverse=True, return_counts=True)
+        indices_per_value = [np.where(inverse_indices == i)[0] for i in range(len(unique_values))]
+        for value,  indices in zip(unique_values, indices_per_value):
+            mapping[value] = list(indices)
+        return mapping
+
+            # print(f"{value}: count={count}, indices={list(indices)}")
+
+
+
--- src/pharming.py
+++ src/pharming.py
@@ -4,7 +4,7 @@ import numpy as np
 from fit_segment_tree import BuildSegmentTree
 import pandas as pd 
 from data import Data
-
+import argparse
         
 def convert_tree_string(edge_string,id):
     tree = nx.DiGraph()
@@ -65,34 +65,164 @@ def read_genotype_trees(fname):
     return genotype_trees 
        
         
+class Pharming:
+    def __init__(self, max_dcf_clusters=5, nrestarts=10) -> None:
+        self.max_clusters = max_dcf_clusters
+        self.nrestarts = nrestarts
+
+    def fit_segment(self, s):
+        seg_snvs = self.data.seg_to_snvs[s]
+        cells_by_cn = self.data.cells_by_cn(s)
+        vaf_by_cn = {}
+        for cn, cells in cells_by_cn.items():
+            print(f"segment: {s} copy number {cn}: #cells {len(cells)}")
+            vaf_by_cn[cn] = self.data.compute_vafs(cells = cells, snvs=seg_snvs)
+            
+            # print(vaf_by_cn[cn])
+
+
+
+    def fit(self, data):
+        self.data = data 
+        self.segments = data.segments
+        for s in self.segments:
+            self.fit_segment(s)
+        print("done")
+
 
 
 if __name__ == "__main__":
 
-    fname = "/scratch/data/leah/phertilizer2.0/src/test_state_trees.txt"
-    geno_trees = read_genotype_trees(fname)
+    # fname = "/scratch/data/leah/pharming/src/test_state_trees.txt"
+    # geno_trees = read_genotype_trees(fname)
 
 
-    T_CNA = geno_trees[0].generate_CNA_tree()
+    # T_CNA = geno_trees[0].generate_CNA_tree()
 
-    T_SNVs = {0: geno_trees[1], 1: geno_trees[2], 2: geno_trees[3], 3: geno_trees[0]}
-    clusters = np.array([i for i in range(4)], dtype=int)
-    DCF = np.array([[0.855, 0.175, 0, 0.145 ], [1,1,1,0]])
-    cn_profile = np.array([2,2,5,2]).reshape(-1,1)
-    genotypes = np.array([[1,0,0,0], [1,1,0,0],[1,1,1,0], [0,0,0,1]])
+    # T_SNVs = {0: geno_trees[1], 1: geno_trees[2], 2: geno_trees[3], 3: geno_trees[0]}
+    # clusters = np.array([i for i in range(4)], dtype=int)
+    # DCF = np.array([[0.855, 0.175, 0, 0.145 ], [1,1,1,0]])
+    # cn_profile = np.array([2,2,5,2]).reshape(-1,1)
+    # genotypes = np.array([[1,0,0,0], [1,1,0,0],[1,1,1,0], [0,0,0,1]])
 
-    var =np.full_like(genotypes, 0)
-    var[genotypes==1] = 5
-    total = np.full_like(genotypes, 10)
-    snvs = np.array([i  for i in range(4)], dtype=int)
-    cell_lookup = pd.Series([i for i in range(4)])
-    mut_lookup =pd.Series([i for i in range(4)])
-    snv_to_seg = np.full_like(snvs, 1)
+    # var =np.full_like(genotypes, 0)
+    # var[genotypes==1] = 5
+    # total = np.full_like(genotypes, 10)
+    # snvs = np.array([i  for i in range(4)], dtype=int)
+    # cell_lookup = pd.Series([i for i in range(4)])
+    # mut_lookup =pd.Series([i for i in range(4)])
+    # snv_to_seg = np.full_like(snvs, 1)
 
-    data = Data(var, total, cn_profile, snv_to_seg, cell_lookup, mut_lookup)
-    cells_by_sample= {0: [0,1,3], 1: [2]}
-    cell_assign = BuildSegmentTree(0,T_CNA, T_SNVs, DCF, clusters, cells_by_sample, data ).fit()
+    # data = Data(var, total, cn_profile, snv_to_seg) #cell_lookup, mut_lookup)
+    # cells_by_sample= {0: [0,1,3], 1: [2]}
+    # cell_assign = BuildSegmentTree(0,T_CNA, T_SNVs, DCF, clusters, cells_by_sample, data ).fit()
 
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", required=True,
+                        help="input file for variant and total read counts with unlabled columns: [chr segment snv cell var total]")
+    parser.add_argument("-c" ,"--copy_numbers", required=True,
+                        help="input files of copy numbers by segment with unlabeled columns [segment cell totalCN]")
+    tpath = "/scratch/data/leah/pharming/test"
+    args = parser.parse_args([
+        "-f", f"{tpath}/input/read_counts.tsv",
+        "-c", f"{tpath}/input/copy_numbers.tsv"
+    ])
+
+    print("\nWelcome to the Pharm! Let's start pharming.....\n")
+    col_names = ['chr', 'segment', 'mutation_label', 'cell_label','var', 'total']
+
+
+    
+    read_counts = pd.read_table(
+        args.file, sep="\t", header=None, names=col_names, skiprows=[0])
+    
+    read_counts['chr_mutation'] = read_counts['chr'].astype('str') + "_" + \
+             read_counts['mutation_label'].astype(str)
+    
+
+    cell_labels = np.sort(read_counts['cell_label'].unique())
+    mut_labels = np.sort(read_counts['chr_mutation'].unique())
+
+
+
+    #create indexed series of mapping of cell index to label
+    cell_lookup = pd.Series(data=cell_labels, name="cell_label").rename_axis("cell")     
+    mut_lookup = pd.Series(data=mut_labels, name="chr_mutation").rename_axis("mut")
+
+
+
+    read_counts = pd.merge(read_counts, cell_lookup.reset_index(), on='cell_label', how='left')
+    read_counts = pd.merge(read_counts, mut_lookup.reset_index(), on='chr_mutation', how='left')
+   
+
+    copy_numbers = pd.read_table(args.copy_numbers, names=["segment", "cell_label", "cn"])
+    segs = copy_numbers.loc[:, ["segment"]].drop_duplicates()
+    seg_labels = np.sort(segs['segment'].unique())
+    seg_lookup = pd.Series(data=seg_labels, name="segment").rename_axis("seg_id")
+
+    copy_numbers = pd.merge(copy_numbers, cell_lookup.reset_index(), on='cell_label', how='left').drop("cell_label", axis=1)
+    copy_numbers= pd.merge(copy_numbers, seg_lookup.reset_index(), on='segment', how='left').drop("segment", axis=1)
+
+
+    
+    read_counts = pd.merge(read_counts, seg_lookup.reset_index(), on='segment', how='left').drop("segment", axis=1)
+    seg_to_mut_mapping = read_counts.loc[:, ["seg_id", "mut"]].drop_duplicates()
+    snv_to_seg = seg_to_mut_mapping.set_index("mut")["seg_id"].to_dict()
+    seg_to_snvs =  {value: [k for k, v in snv_to_seg.items() if v == value] for value in set(snv_to_seg.values())}
+
+
+    #check that the copy number data frame contains the exact same cells as read counts
+    # cn_cells_in_read_counts = copy_numbers['cell'].isin(read_counts['cell']).all()
+    # if cn_cells_in_read_counts.all():
+        
+    #     print(f"checked that both input datasets contains the same {cell_lookup.shape[0]} cells")
+      
+    # else:
+    #     print("cells appear in copy number input that are not in read counts input these cells will be dropped")
+  
+    
+
+    
+
+    read_counts= read_counts.set_index(["cell", "mut"])
+    var = read_counts["var"].unstack(level="mut", fill_value=0).to_numpy()
+    total = read_counts["total"].unstack(level="mut", fill_value=0).to_numpy()
+    copy_numbers = copy_numbers.set_index(["seg_id", "cell"])
+    copy_numbers= copy_numbers.unstack(level="seg_id", fill_value=0).to_numpy()
+
+    dat = Data(var, total, copy_numbers, snv_to_seg, seg_to_snvs)
+    Pharming().fit(dat)
+
+
+
+ 
+
+
+ 
+    
+    #create cell indices for 
+
+            #wrangle input data into the correct format
+       
+        
+
+       
+  
+
+
+        
+        # variant_count_data= variant_count_data.set_index(["cell", "mutation"])
+
+
+ 
+          
+ 
+        # bin_count_data['cell_index'] = cell_series[bin_count_data['cell']].values
+        # bin_count_data = bin_count_data.sort_values(by=['cell_index'])
+
+        # bin_count_data.drop(['cell', 'cell_index'], inplace=True, axis=1)
+        # bin_count_data = bin_count_data.to_numpy()
     
 
 
