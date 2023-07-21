17bb3886bd4fad8b57864b908a32232389eebd11
Leah Weber <leahlw2@ada.cs.illinois.edu>
2023-07-20 19:21:47 -0500

    fixed bug in identifying the root of subtree to enumerate candidate trees at the leaf nodes

--- src/pharming.py
+++ src/pharming.py
@@ -2,11 +2,14 @@ from genotype_tree import CNATree
 import networkx as nx 
 import numpy as np
 from fit_segment_tree import BuildSegmentTree
-from data import load, Data
+from data import load_from_files, load_from_pickle
 import argparse
 from clonal_tree import ClonalTree
 import os 
 import itertools
+import multiprocessing
+import cProfile
+
 
        
         
@@ -72,13 +75,18 @@ class Pharming:
             # cn_states = [(1,1), (3,1), (4,1)]
         T_CNAs = self.get_cna_trees(cn_states)
         best_like = np.NINF
-        for T_CNA in T_CNAs:
- 
-            SegTree =BuildSegmentTree(T_CNA).fit(self.data, g)
 
-            if SegTree.loglikelihood > best_like:
-                best_like = SegTree.loglikelihood
-                BestSegTree= SegTree
+        for T_CNA in T_CNAs:
+            seed = self.rng.integers(1e8, size=1)[0]
+            try:
+                SegTree =BuildSegmentTree(T_CNA,seed).fit(self.data, g)
+
+                if SegTree.loglikelihood > best_like:
+                    best_like = SegTree.loglikelihood
+                    BestSegTree= SegTree
+            except:
+                print(f"Warning: Segment {g} failed")
+               
         
         return BestSegTree
         
@@ -126,11 +134,33 @@ class Pharming:
                 print(f"Segment {g} complete!")
             # except:
            
-                # print(f"Warning: Segment {g} failed!")
-                # self.SegTrees[g]= None
+            #     print(f"Warning: Segment {g} failed!")
+            #     self.SegTrees[g]= None
            
         
         self.combine_trees()
+        return self.SegTrees
+    
+    def fit_parallel(self, data, segments= None, num_cores=4):
+        
+        self.data = data
+          
+        if segments is None:
+            segments = data.segments
+      
+     
+    
+        pool = multiprocessing.Pool(processes=num_cores)
+
+        # Map the build_segment_tree function to the segments using the pool
+        results = pool.map(self.fit_segment, segments)
+
+        # Close the pool and wait for all processes to complete
+        pool.close()
+        pool.join()
+
+        self.SegTrees = {g: T_Seg for g,T_Seg in zip(segments,results)}
+        
         return self.SegTrees
 
 
@@ -149,7 +179,9 @@ class Pharming:
 
 
 
-     
+# def my_function():
+#      _ = Pharming(args.seed).fit(dat, [0])
+#      return 
 
   
 
@@ -157,60 +189,87 @@ if __name__ == "__main__":
 
   
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--file", required=True,
+    parser.add_argument("-d", "--data", required=False,
+                        help="input file of preprocessed data pickle")
+
+    parser.add_argument("-f", "--file", required=False,
                         help="input file for variant and total read counts with unlabled columns: [chr segment snv cell var total]")
-    parser.add_argument("-c" ,"--copy-numbers", required=True,
+    parser.add_argument("-c" ,"--copy-numbers", required=False,
                         help="input files of copy numbers by segment with unlabeled columns [segment cell totalCN]")
     parser.add_argument("-s" ,"--seed", required=False, type=int,
                         help="random number seed (default: 1026)")
-
+    
+    parser.add_argument("-g" ,"--segment", required=False, type=int,
+                        help="segment id of tree to build")
     parser.add_argument("-o" ,"--out", required=False, type=str,
                         help="directory where output files should be written")
+    parser.add_argument("-L", "--likelihoods", type=str,
+        help = "filename of marginal likelihood")
     # parser.add_argument("--state-trees", required=False, 
     #                     help= "filename of pregenerated state trees or path of program to generate state trees" )
     
-    parser.add_argument('-g', '--segment', type=int, required=False)
-    tpath = "/scratch/data/leah/pharming/test"
-    args = parser.parse_args([
-        "-f", f"{tpath}/input/read_counts.tsv",
-        "-c", f"{tpath}/input/copy_numbers.tsv",
-        "-s", "11",
-        "--segment", "5",
-        "--out", "test/Seg5"
-        # "--state-trees", "/scratch/data/leah/pharming/src/test_state_trees.txt"
-        # "--state-trees", "/scratch/data/leah/pharming/decifer/build/generatestatetrees"
-    ])
+    args = parser.parse_args()
+    # parser.add_argument('-g', '--segment', type=int, required=False)
+    # parser.add_argument("-d", "--data", type=str)
+    
+    # tpath = "/scratch/data/leah/pharming/sim_study/pharming/s12_n1000_m15000_c5_p0.05_l0"
+    # args = parser.parse_args([
+    #     # "-f", f"{tpath}/input/read_counts.tsv",
+    #     # "-c", f"{tpath}/input/copy_numbers.tsv",
+    #     "-d", f"{tpath}/data.pickle",
+    #     "-s", "13",
+    #     "--segment", "1",
+    #     "--out", f"{tpath}/SegTrees",
+    #     "-L", f"{tpath}/like.csv"
+    #     # "--state-trees", "/scratch/data/leah/pharming/src/test_state_trees.txt"
+    #     # "--state-trees", "/scratch/data/leah/pharming/decifer/build/generatestatetrees"
+    # ])
 
 
     print("\nWelcome to the Pharm! Let's start pharming.....\n")
     
-
-    dat = load(args.file, args.copy_numbers )
+    if args.data is not None:
+        dat = load_from_pickle(args.data)
+    elif args.file and args.copy_numbers is not None:
+        dat = load_from_files(args.file, args.copy_numbers )
+    else:
+        IOError("Either both read counts and copy number files must be specified or alternatively, the path to the preprocessed pharming data object!")
+    
     if args.segment is None:
         segments = dat.segments
     
     else:
         segments = [args.segment]
+    # segments = segments[:4]
+    # SegTrees = Pharming(args.seed).fit_parallel(dat, segments,num_cores=10)
     SegTrees = Pharming(args.seed).fit(dat, segments)
+    # SegTrees ={}
+    # cProfile.run("my_function()")
+
     if not os.path.exists(args.out):
         os.makedirs(args.out)
         print(f"Directory '{args.out}' created successfully.")
     else:
         print(f"Directory '{args.out}' already exists.")
+    likelihoods = {}
     for g, T_Seg in SegTrees.items():
         if T_Seg is not None:
             pred_cell, pred_mut = T_Seg.generate_results(dat.cell_lookup, dat.mut_lookup)
-
-            print(pred_cell.head())
-            print(pred_mut.head())
-            pred_cell.to_csv(f"{args.out}/pred_cell.csv", index=False)
-            pred_mut.to_csv(f"{args.out}/pred_mut.csv",index=False)
-            T_Seg.draw(f"{args.out}/tree.png")
-            T_Seg.save(f"{args.out}/tree.pickle")
-            T_Seg.save_text(f"{args.out}/tree.txt")
+            likelihoods[g]= T_Seg.compute_likelihood(dat,g)
+            pred_cell.to_csv(f"{args.out}/pred_cell_g{g}.csv", index=False)
+            pred_mut.to_csv(f"{args.out}/pred_mut_g{g}.csv",index=False)
+            T_Seg.draw(f"{args.out}/tree_g{g}.png")
+            T_Seg.save(f"{args.out}/tree_g{g}.pickle")
+            T_Seg.save_text(f"{args.out}/tree__g{g}.txt")
 
 
-
+    if args.likelihoods is not None:
+            print("Saving segment likelihoods...")
+            with open(args.likelihoods, "w+") as file:
+                file.write("segment,likelihood\n")
+                for g, like in likelihoods.items():
+                    file.write(f"{g},{like}\n")
+     
 
 
 
@@ -220,9 +279,7 @@ if __name__ == "__main__":
 
  
     
-    #create cell indices for 
 
-            #wrangle input data into the correct format
        
         
 
@@ -231,18 +288,7 @@ if __name__ == "__main__":
 
 
         
-        # variant_count_data= variant_count_data.set_index(["cell", "mutation"])
-
-
  
-          
- 
-        # bin_count_data['cell_index'] = cell_series[bin_count_data['cell']].values
-        # bin_count_data = bin_count_data.sort_values(by=['cell_index'])
-
-        # bin_count_data.drop(['cell', 'cell_index'], inplace=True, axis=1)
-        # bin_count_data = bin_count_data.to_numpy()
-    
 
 
     
