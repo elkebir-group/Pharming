549137eaf2f968e05ca97ce1aa46cb292bb4f919
Leah Weber <leahlw2@ada.cs.illinois.edu>
2023-09-05 15:33:35 -0500

    ILP to solve the tree merge problem

--- src/superimposition.py
+++ src/superimposition.py
@@ -1,74 +1,108 @@
-import gurobipy as gp
+import gurobipy as gp 
+from gurobipy import GRB
 import numpy as np
 
 import networkx as nx
-import itertools
+
 
 
 class Superimposition:
-    def __init__(self, G:nx.digraph, T1:nx.digraph, T2:nx.digraph,  threads = 1, timelimit = None, verbose = True) -> None:
-        self.G = G 
+    def __init__(self,T1, T2, verbose = True) -> None:
+        
         self.T1 = T1
-        self.T2 = T2 
-        self.threads = threads 
-        self.timelimit = timelimit
+        self.T2 = T2
+        self.T1_root =self.T1.find_root()
+        self.T2_root = self.T2.find_root()
+
         self.verbose  = verbose 
 
-        self.T1_clones = list(self.T1.nodes) 
-        self.T2_clones = list(self.T2.nodes)
-        self.K1 = len(self.T1)
-        self.K2 = len(self.T2)
-        self.edges = list(self.G.edges)
+        self.T1_clones = self.T1.clones()
+        self.T2_clones = self.T2.clones()
+        self.T1_edges = self.T1.edges()
+        self.T2_edges = self.T2.edges()
 
-    def solve(self):
+
+        self.PI = [ (u,v) for u in self.T1_clones for v in self.T2_clones]
+
+
+
+    def solve(self, data, threads=1, timelimit=500):
+
+        rng = np.random.default_rng(12345)
         model = gp.Model('mergeTrees')
+        if not self.verbose:
+            model.Params.LogToConsole = 0
         
-        x = model.addVars(self.K1, self.K2, vtype=gp.GRB.BINARY, name='x')
-        y = model.addVars(len(self.edges), vtype=gp.GRB.CONTINUOUS, name='y')
-        z1 = model.addVars(len(list(self.T1.edges)), self.K2, vtype=gp.GRB.CONTINUOUS, name='z1')
-        z2 = model.addVars(self.K1, len(list(self.T2.edges)), vtype=gp.GRB.CONTINUOUS, name='z2')
+        model.Params.Threads = threads 
+        model.Params.TimeLimit = timelimit
+        
+        cells = data.cells 
+        
+        x = model.addVars(self.T1_clones, self.T2_clones, vtype=GRB.BINARY, name='x')
+        z1 = model.addVars(self.T1_edges, self.T2_clones, vtype=GRB.CONTINUOUS, name='z1',lb=0, ub=1)
+        z2 = model.addVars(self.T1_clones, self.T2_edges, vtype=GRB.CONTINUOUS, name='z2', lb=0, ub=1)
+        phi = model.addVars(cells, self.T1_clones, self.T2_clones, vtype = GRB.CONTINUOUS, name='phi', lb=0, ub=1)
 
-               # encode z1[j,k] = x[parent(j), k] * x[j,k]
-                #encode z2[j,k] = x[j, parent(k)] * x[j,k]
-        self.t1_edge_index = {}
-        for edge_idx, edge in enumerate(self.T1.edges):
-            parent = edge[0]
-            child = edge[1]
-            self.t1_edge_index[child] = edge_idx
-            for k in range(self.K2):
-             
-                model.addConstr(z1[edge_idx, k] <= x[parent, k])
-                model.addConstr(z1[edge_idx, k] <= x[child, k])
-                model.addConstr(z1[edge_idx, k] >= x[parent, k] + x[child, k] - 1)
         
-        self.t2_edge_index = {}
-        for edge_idx, edge in enumerate(self.T2.edges):
-            parent = edge[0]
-            child = edge[1]
-            self.t2_edge_index[child] = edge_idx
-            for j in range(self.K1):
-                model.addConstr(z2[j, edge_idx] <= x[j, parent])
-                model.addConstr(z2[j, edge_idx] <= x[j, child])
-                model.addConstr(z2[j, edge_idx] >= x[j, parent] + x[j, child] - 1)
+        for i in cells: 
+            #every cell is assigned to exactly one clone
+            model.addConstr(sum(phi[i,u,v] for u,v in self.PI)==1)
+            
+            #cells can only be assigned t o clones included in the refined tree
+            model.addConstrs(phi[i,u,v] <= x[u,v] for u,v in self.PI)
+
+        #tree refinement constraints
+        for v in self.T2_clones:
+            for u, u_prime in self.T1_edges:
+            
+                model.addConstr(z1[u, u_prime,v] <= x[u,v])
+                model.addConstr(z1[u, u_prime,v] <= x[u_prime, v])
+                model.addConstr(z1[u, u_prime,v] >= x[u_prime, v] + x[u_prime, v] - 1)
         
+        for u in self.T1_clones:
+            for v, v_prime in self.T2_edges:
+  
+                model.addConstr(z2[u,v, v_prime] <= x[u,v])
+                model.addConstr(z2[u,v, v_prime] <= x[u, v_prime])
+                model.addConstr(z2[u,v, v_prime] >= x[u,v] + x[u, v_prime] - 1)
+
+
+        for u, u_prime in self.T1_edges:
+            model.addConstr(sum(z1[u, u_prime,v] for v in self.T2_clones) ==1)
         
-        for i, i_clone in enumerate(self.T1_clones):
-            for j, j_clone in enumerate(self.T2_clones):
-                out = (0,i_clone)
-                in_ = (1,j_clone)
-                j_edge_index = self.t2_edge_index[j]
-                if (out, in_) in self.G.edges:
-                    model.addConstr(y[i,j]==z2[i, j_edge_index])
-    
+        for v, v_prime in self.T2_edges:
+            model.addConstr(sum(z2[u,v, v_prime] for u in self.T1_clones) ==1)
         
-        for i, i_clone in enumerate(self.T1_clones):
-            for j, j_clone in enumerate(self.T2_clones):
-                out = (0,i_clone)
-                in_ = (1,j_clone)
-                i_edge_index = self.t1_edge_index[i]
-                if (out, in_) in self.G.edges:
-                    model.addConstr(y[j,i]==z1[i_edge_index, j])
+        c = {(i,u,v): rng.random() for i in cells for u,v in self.PI}
+
+
+        model.setObjective( sum(c[i,u,v]* phi[i,u,v] for i in cells for u,v in self.PI) , GRB.MINIMIZE)
+
+        #minimize 
+        model.optimize()
+        if model.Status == GRB.OPTIMAL:
+                solution = model.getAttr('X', x)
+                phi = model.getAttr('X', phi)
+                score = model.objVal
+
+                print(f"score: {score}")
+                
+
+       
+         
+      
         
+   
+
+                     
+
+  
+  
+
+
+        
+
+
 
 
 
