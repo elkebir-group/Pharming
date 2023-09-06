ecab9c9979529532b638089299e5602275dae1ce
Leah Weber <leahlw2@ada.cs.illinois.edu>
2023-09-05 22:11:34 -0500

    fixed cost function

--- src/superimposition.py
+++ src/superimposition.py
@@ -1,7 +1,7 @@
 import gurobipy as gp 
 from gurobipy import GRB
 import numpy as np
-
+from clonal_tree_new import ClonalTreeNew
 import networkx as nx
 
 
@@ -20,6 +20,10 @@ class Superimposition:
         self.T2_clones = self.T2.clones()
         self.T1_edges = self.T1.edges()
         self.T2_edges = self.T2.edges()
+
+        self.T = nx.DiGraph()
+        self.genotypes = {}
+        self.cell_mapping = {}
 
 
         self.PI = [ (u,v) for u in self.T1_clones for v in self.T2_clones]
@@ -73,19 +77,76 @@ class Superimposition:
         for v, v_prime in self.T2_edges:
             model.addConstr(sum(z2[u,v, v_prime] for u in self.T1_clones) ==1)
         
-        c = {(i,u,v): rng.random() for i in cells for u,v in self.PI}
+        cst ={}
+        for u in self.T1_clones:
+            u_costs = self.T1.node_snv_cost(u,cells, data)
+            for v in self.T2_clones:
+                v_costs = self.T2.node_snv_cost(v, cells, data)
+                tot = u_costs + v_costs 
+                for i,c in enumerate(cells):
+                    cst[c,u,v]  = tot[i] 
+
+            
+        
+        # c = {(i,u,v): rng.random() for i in cells for u,v in self.PI}
 
 
-        model.setObjective( sum(c[i,u,v]* phi[i,u,v] for i in cells for u,v in self.PI) , GRB.MINIMIZE)
+        model.setObjective( sum(cst[i,u,v]* phi[i,u,v] for i in cells for u,v in self.PI) , GRB.MINIMIZE)
 
-        #minimize 
+        #solve ILP
         model.optimize()
+        
         if model.Status == GRB.OPTIMAL:
-                solution = model.getAttr('X', x)
-                phi = model.getAttr('X', phi)
+                clones= model.getAttr('X', x)
+                cell_assign = model.getAttr('X', phi)
                 score = model.objVal
 
                 print(f"score: {score}")
+                mapping = {}
+                orig_clones = []
+                index  =0 
+                for u,v in self.PI:  
+                    if clones[u,v] > 0.5:
+                        self.cell_mapping[index] = []
+                        mapping[(u,v)] = index
+                        orig_clones.append((u,v))
+                        self.T.add_node(index)
+                        #TODO: fix seg ids 
+                        self.genotypes[index] = {self.T2.key: self.T2.genotypes[v][self.T2.key],self.T1.key: self.T1.genotypes[u][self.T1.key] }
+                        for i in cells:
+                            if cell_assign[i,u,v] > 0.5:
+                                self.cell_mapping[index].append(i)
+                        index+=1
+                
+          
+                for u,v in orig_clones:
+                    
+                    if u == self.T1_root and v ==self.T2_root:
+                        continue
+                    u_prime = self.T1.parent(u)
+                    v_prime = self.T2.parent(v)
+                    if (u_prime, v) in orig_clones:
+                        self.T.add_edge(mapping[u_prime,v], mapping[u,v])
+                    else:
+                        self.T.add_edge(mapping[u, v_prime], mapping[u,v])
+
+
+
+
+
+
+        else:
+            raise ValueError("Model is Infeasible!")
+
+        self.CT = ClonalTreeNew(self.T, self.genotypes, self.cell_mapping, key=(self.T1.key, self.T2.key), cost=score)
+        return score, self.CT, mapping 
+    
+
+            
+   
+
+
+
                 
 
        
